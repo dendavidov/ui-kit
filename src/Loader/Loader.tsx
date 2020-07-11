f513880d5d924c9e09247fcca2ee7d9e17a900b2
Denis Davydov <mail@dendavidov.com>
2020-07-11 22:25:15 +0200

    Refactor Loader

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,16 +1,7 @@
 import * as React from 'react';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() => ({
-  root: {
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
+import { useStyles } from './Loader.styles';
 
 const Loader = () => {
   const classes = useStyles();
